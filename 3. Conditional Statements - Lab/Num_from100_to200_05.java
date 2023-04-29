bc1a19eabcfb74ebf6b5b8fa516e1a377cb9ff23
iliqkolev <122911131+iliqkolev@users.noreply.github.com>
2023-04-29 16:16:30 +0300

    Add files via upload

---
+++ 3. Conditional Statements - Lab/Num_from100_to200_05.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Num_from100_to200_05 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        Integer number=Integer.parseInt(scanner.nextLine());
+        if (number < 100)
+        {
+            System.out.println("Less than 100");
+        }
+        else if ( number <=200)
+        {
+            System.out.println("Between 100 and 200");
+        }
+        else
+        {
+            System.out.println("Greater than 200");
+        }
+    }
+}
